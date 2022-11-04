500d8ced124cceb78fe1c703d0f8906152cecaa3
Tarakanov Vyacheslav <kriodezz@gmail.com>
2022-11-05 02:21:00 +0500

    add service

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -3,25 +3,16 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'image' => 'string',
-            'content' => 'required|string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
+        $data = $request->validated();
 
-        $tags = $data['tags'];
-        unset($data['tags']);
-
-        $post = Post::create($data);
-        $post->tags()->attach($tags);
+        $this->service->store($data);
 
         return redirect()->route('post.index');
     }
